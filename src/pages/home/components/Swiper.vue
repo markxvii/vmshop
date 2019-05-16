44ff806cd2f46a1ea805985956914862ff0ebee6
Marc <markxvii@outlook.com>
2019-05-16 19:38:06 +0800

    index swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+			<swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-img" :src="item.imgUrl"
+							   alt=""></swiper-slide>
+			<div class="swiper-pagination" slot="pagination"></div>
+		</swiper>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "HomeSwiper",
+		data() {
+			return {
+				swiperOption: {
+					pagination:'.swiper-pagination',
+					loop:true,
+				},
+				swiperList:[{
+					id:'0001',
+					imgUrl:"//c3.xinstatic.com//c/20190128/1516/5c4eac64729c7945932.png"
+				},{
+					id:'0002',
+					imgUrl:"//c3.xinstatic.com//c/20181010/1903/5bbddc6beefcb701176.png"
+				}]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	.wrapper >>> .swiper-pagination-bullet-active
+		background red !important
+
+	.wrapper
+		overflow:hidden
+		width 100%
+		height 0
+		padding-bottom 42.67%
+		.swiper-img
+			width 100%
+
+</style>
