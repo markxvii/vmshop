45172ac1e80f20bdd3bea5d9eb7e803be3b48d8e
Marc <markxvii@outlook.com>
2019-05-17 17:34:23 +0800

    index icons

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,74 @@
+<template>
+	<div>
+		<div class="title">查看详情</div>
+		<ul>
+			<li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+				<img class="item-img"
+					 :src="item.imgUrl"
+					 alt="">
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+					<button class="item-button">查看详情</button>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "HomeRecommend",
+		data() {
+			return {
+				recommendList: [
+					{id:'0001',imgUrl:'https://img1.qunarzz.com/p/tts3/1603/6e/649f8db09ea57bf7.jpg_r_240x160x90_02a57254.jpg',title:'斯文这下牛逼了',desc:'攻击力是现在的两倍'},
+					{id:'0002',imgUrl:'https://img1.qunarzz.com/p/tts3/1603/6e/649f8db09ea57bf7.jpg_r_240x160x90_02a57254.jpg',title:'斯文这下牛逼了',desc:'攻击力是现在的两倍'},
+					{id:'0003',imgUrl:'https://img1.qunarzz.com/p/tts3/1603/6e/649f8db09ea57bf7.jpg_r_240x160x90_02a57254.jpg',title:'斯文这下牛逼了',desc:'攻击力是现在的两倍'},
+				]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/mixins.styl'
+	.title
+		margin-top .2rem
+		line-height: .4rem
+		background #eee
+		text-indent .2rem
+
+	.item
+		overflow: hidden
+		display flex
+		height: 1.9rem
+
+		.item-img
+			width: 1.7rem
+			height: 1.7rem
+			padding: .1rem
+
+		.item-info
+			flex: 1
+			min-width 0
+
+			.item-title
+				line-height .54rem
+				font-size .32rem
+				ellipsis()
+
+			.item-desc
+				line-height .4rem
+				color: #ccc
+				ellipsis()
+
+			.item-button
+				line-height .44rem
+				margin-top .16rem
+				color #fff
+				background #ff9300
+				border-radius .06rem
+				padding 0 .2rem
+
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,78 @@
+<template>
+	<div>
+		<div class="title">特价推荐</div>
+		<ul>
+			<li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+				<div class="item-img-wrapper">
+					<img class="item-img"
+						 :src="item.imgUrl"
+						 alt="">
+				</div>
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "HomeWeekend",
+		data() {
+			return {
+				recommendList: [
+					{
+						id: '0001',
+						imgUrl: 'https://img1.qunarzz.com/vs_ceph_vs_tts/4051abb5-37db-43ec-aaf6-364770ad6dfb.jpg_r_1280x840x90_8e63e920.jpg',
+						title: '斯文这下牛逼了',
+						desc: '攻击力是现在的两倍'
+					},
+					{
+						id: '0002',
+						imgUrl: 'https://img1.qunarzz.com/vs_ceph_vs_tts/4051abb5-37db-43ec-aaf6-364770ad6dfb.jpg_r_1280x840x90_8e63e920.jpg',
+						title: '斯文这下牛逼了',
+						desc: '攻击力是现在的两倍'
+					},
+					{
+						id: '0003',
+						imgUrl: 'https://img1.qunarzz.com/vs_ceph_vs_tts/4051abb5-37db-43ec-aaf6-364770ad6dfb.jpg_r_1280x840x90_8e63e920.jpg',
+						title: '斯文这下牛逼了',
+						desc: '攻击力是现在的两倍'
+					},
+				]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/mixins.styl'
+	.title
+		margin-top .2rem
+		line-height: .4rem
+		background #eee
+		text-indent .2rem
+
+	.item-img-wrapper
+		overflow hidden
+		height 0
+		padding-bottom 50%
+		.item-img
+			width 100%
+
+	.item-info
+		padding .1rem
+
+		.item-title
+			line-height .54rem
+			font-size .32rem
+			ellipsis()
+
+		.item-desc
+			line-height .4rem
+			color: #ccc
+			ellipsis()
+
+</style>
